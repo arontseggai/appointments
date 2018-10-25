bbdfacd7e50608309df3756d38cc0cbe72f5ce75
arontseggai <aron.tseggai@gmail.com>
2018-10-25 18:10:59 +0200

    Adds link to room create route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,13 +43,16 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a href="{!! action('RoomController@create'); !!}" class="nav-link">Create Room</a>
                             </li>
                             <li class="nav-item">
                                 @if (Route::has('register'))
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 @endif
                             </li>
+                            <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
